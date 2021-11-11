e57e7906fbdc78bec3f3859ff526850d1996dfea
jansenosorio <93011027+jansenosorio@users.noreply.github.com>
2021-11-11 20:02:19 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { RespositoryList } from './components/RepositoryList';
+import './styles/global.scss';
+
+export function App() {
+  return (
+   
+    <RespositoryList />
+    
+   
+  )
+}
